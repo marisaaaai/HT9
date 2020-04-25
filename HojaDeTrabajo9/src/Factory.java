fa7f7aef95e7a6beaa7aa4887722cf497dca079c
marisaamv <mon19169@uvg.edu.gt>
2020-04-25 08:59:18 -0600

    Cambios por errores, sigue existiendo un error en clase Factory

--- HojaDeTrabajo9/src/Factory.java
+++ HojaDeTrabajo9/src/Factory.java
@@ -1,22 +1,24 @@
+  
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-
+import java.util.*; 
+import java.util.concurrent.*; 
 /*
  * @author Maria Isabel Montoya Valladares, Javier Alejandro Cotto Argueta
  * @carne 19169, 19324
  * @date 26/04/2020
  * @name Factory.java
  * */
-public class Factory {
-	 public static Map<String, String[]> obtenerMap(String opcion){
-	        switch (opcion){
-	            case "1":
-	                return new HashMap<>();
-	            case "2":
-	                return new TreeMap<>();
-	            default:
-	                return null;
-	        }
+public class Factory<K,V> {
+	public map<K,V> obtenerMap(int opcion){
+	    switch (opcion){
+	        case 1:
+	            return new hashMap<K,V>();
+	        case 2:
+	            return new RedBlackTree<K,V>();
+	        default:
+	            return null;
 	    }
+	}
 }
